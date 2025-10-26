a84f69f8a05b75ed3a4da2c43e60f5931416778d
SingaBAung <7afajarihsan@gmail.com>
2025-10-26 22:14:16 +0700

    salah framework

--- backend/prisma/migrations/20251026125559_penambahan_reason/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "TokenBlacklist" ADD COLUMN     "reason" TEXT DEFAULT 'MANUAL_LOGOUT';
-
--- CreateIndex
-CREATE INDEX "TokenBlacklist_expiresAt_idx" ON "TokenBlacklist"("expiresAt");
